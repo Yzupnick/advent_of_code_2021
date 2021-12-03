569661294348a28bfaea447c25e430569f83b029
Chanan Zupnick <chanan@heythere.email>
2021-12-03 13:06:46 -0800

    Day 3 part one test passes

---
+++ day3-part1/src/main.rs
@@ -0,0 +1,139 @@
+use std::io;
+use std::io::BufRead;
+
+
+fn main() {
+    let diagnostic  = 
+        io::stdin().lock().lines()
+                          .map(readline_or_panic)
+                          .map(parse_point_or_panic)
+                          .fold(Diagnosticts::new(), index_diagnostic_data);
+    println!("{}",diagnostic.power_comsumption());
+}
+
+// state is stored as an array of BitCounts, index in array is equal to the power of the column.
+type IndexedDiagnosticData = [BitCount; usize::BITS as usize];
+
+struct Diagnosticts {
+    data: IndexedDiagnosticData,
+    size: Option<usize>,
+}
+
+impl Diagnosticts {
+    fn new() -> Diagnosticts {
+        Diagnosticts {
+            data: [BitCount::new(); usize::BITS as usize],
+            size: None
+        }
+    }
+    fn gamma_rate(&self) -> usize {
+        return most_common_bits(self.data);
+    }
+
+    fn epsilon_rate(&self) -> usize {
+        // 0000 1010
+        // 0000 0100
+        // 0000 1000
+        // 1111 0111
+        // 0000 1111
+        let common_bits = most_common_bits(self.data);
+        if self.size.is_none() {
+            println!("Something went wrong, diagnostic size is set to None");
+            std::process::exit(1);
+        } else {
+            let size = self.size.unwrap();
+            let inverted = size ^ usize::MAX;
+            let mask = inverted >> (usize::BITS - (size as u32));
+            return common_bits ^ mask;
+        }
+    }
+
+    fn power_comsumption(self) -> usize {
+        self.gamma_rate() * self.epsilon_rate()
+    }
+
+}
+
+struct DiagnosticDataPoint {
+    value: usize,
+    size: usize
+}
+
+#[derive(Default, Clone, Copy)]
+struct BitCount {
+    zeros: u32,
+    ones: u32,
+}
+
+impl BitCount {
+    fn add_one(self) -> BitCount {
+        BitCount {
+            zeros: self.zeros,
+            ones: self.ones + 1,
+        }
+    }
+    fn add_zero(self) -> BitCount {
+        BitCount {
+            zeros: self.zeros + 1,
+            ones: self.ones,
+        }
+    }
+    fn new() -> BitCount {
+        BitCount { zeros: 0, ones: 0}
+    }
+}
+
+fn index_diagnostic_data (mut state: Diagnosticts, next:DiagnosticDataPoint) ->  Diagnosticts {
+    if state.size.is_none() {
+        state.size = Some(next.size);
+    }
+    for power in 0..usize::BITS as usize{
+        let cmp = 2^power;
+        let count = &state.data[power];
+        if next.value & cmp == cmp {
+            state.data[power] = count.add_one()
+        } else {
+            state.data[power] = count.add_zero()
+        }
+    }
+    return state;
+}
+
+fn most_common_bits(diagnostic:IndexedDiagnosticData) -> usize {
+    let mut current = 0;
+    for (power, count) in diagnostic.iter().enumerate() {
+        if count.ones > count.zeros {
+            current = current | 2 ^ power;
+        }
+    }
+    return current;
+}
+
+// Returns tuple of size of input and the parsed number.
+fn parse_point_or_panic(input: String) -> DiagnosticDataPoint {
+    let size = input.len();
+    match usize::from_str_radix(&input, 2) {
+        Ok(n) => {
+            return DiagnosticDataPoint {
+                size: size,
+                value: n
+            };
+        }
+        Err(_) => {
+            println!("Error parsing line {}", input);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn readline_or_panic(line:Result<String, std::io::Error>) -> String {
+    match line {
+        Ok(input) => {
+            input
+        }
+        Err(_) => {
+            println!("Error reading from stdin");
+            std::process::exit(1);
+        }
+    }
+}
